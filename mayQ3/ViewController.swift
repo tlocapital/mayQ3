968b59518c119979e18265ef2b5b1f31135d35e3
Ted L <jllts@hotmail.com>
2016-05-17 21:35:08 +0800

    functions

--- mayQ3/ViewController.swift
+++ mayQ3/ViewController.swift
@@ -12,11 +12,40 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("\(#function)")
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+
+    
+    override func viewDidLayoutSubviews() {
+        print("\(#function)")
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        print("\(#function)")
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        print("\(#function)")
+    }
+    
+    
+    override func viewDidDisappear(animated: Bool) {
+        print("\(#function)")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        print("\(#function)")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        print("\(#function)")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        print("\(#function)")
         // Dispose of any resources that can be recreated.
     }
 
